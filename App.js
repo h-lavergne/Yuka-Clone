b2a221febcbcb4ac788d19955d6910583691e5b9
Lavergne <hugo.lavergne@devinci.fr>
2020-01-07 23:27:33 +0100

    camera ok + can scan code barre

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, Button } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as Permissions from 'expo-permissions';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { FontAwesome, Ionicons,MaterialCommunityIcons } from '@expo/vector-icons';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 
 
@@ -30,85 +31,49 @@ class SettingsScreen extends React.Component {
 
 
 
-
-class OpenedCamera extends React.Component
-{
-    state = {
-        hasPermission: null,
-        type: Camera.Constants.Type.back,
-      }
-    
-      
-      async componentDidMount() {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA);
-        this.setState({ hasPermission: status === 'granted' });
-      }
-
-      handleCameraType=()=>{
-        const { cameraType } = this.state
-    
-        this.setState({cameraType:
-          cameraType === Camera.Constants.Type.back
-          ? Camera.Constants.Type.front
-          : Camera.Constants.Type.back
-        })
-      }
-
-      takePicture = async () => {
-        if (this.camera) {
-          let photo = await this.camera.takePictureAsync();
-        }
-      }
-
-      render(){
-        const { hasPermission } = this.state
-        if (hasPermission === null) {
-          return <View />;
-        } else if (hasPermission === false) {
-          return <Text>No access to camera</Text>;
-        } else {
-          return (
-              <View style={{ flex: 1 }}>
-                <Camera style={{ flex: 1 }} type={this.state.cameraType}>
-                
-                <View style={{flex:1, flexDirection:"row",justifyContent:"space-evenly",margin:20}}>
-                    
-                    <TouchableOpacity
-                    style={{
-                        alignSelf: 'flex-end',
-                        alignItems: 'center',
-                        backgroundColor: 'transparent',
-                    }}
-                    onPress={()=>this.handleCameraType()}
-                    >
-                    <MaterialCommunityIcons
-                        name="camera-switch"
-                        style={{ color: "#fff", fontSize: 40}}
-                    />
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={{
-                        alignSelf: 'flex-end',
-                        alignItems: 'center',
-                        backgroundColor: 'transparent',
-                        }}>
-                        <FontAwesome
-                            name="camera"
-                            style={{ color: "#fff", fontSize: 40}}
-                        />
-                    </TouchableOpacity>
-                    </View>
-                </Camera>
-            </View>
-          );
-        }
-      }
+const OpenedCamera = () => {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+  
+    useEffect(() => {
+      (async () => {
+        const { status } = await BarCodeScanner.requestPermissionsAsync();
+        setHasPermission(status === 'granted');
+      })();
+    }, []);
+  
+    const handleBarCodeScanned = ({ type, data }) => {
+      setScanned(true);
+      alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
+  
+    if (hasPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
     }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    } 
 
-
-
-
+  
+    return(
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'flex-end',
+          }}>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+    
+          {scanned && (
+            <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
+          )}
+        </View>
+    );
+    
+}
 
 
 const TabNavigator = createBottomTabNavigator({
